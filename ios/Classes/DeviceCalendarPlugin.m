129349d58e98e312703677ed5877a90d5b2612ec
Tatsuya Yamaguchi <t-yamaguchi@cloudpack.jp>
2021-09-19 13:54:31 +0900

    xxx-Swift.hが見つからない問題のW/A
    詳細はこのIssueを参照
    flutter/flutter#25676

--- ios/Classes/DeviceCalendarPlugin.m
+++ ios/Classes/DeviceCalendarPlugin.m
@@ -1,5 +1,10 @@
 #import "DeviceCalendarPlugin.h"
+
+#if __has_include(<device_calendar/device_calendar-Swift.h>)
 #import <device_calendar/device_calendar-Swift.h>
+#else
+#import "device_calendar-Swift.h"
+#endif
 
 @implementation DeviceCalendarPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
